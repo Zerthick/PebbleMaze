b15e9669385f87efd0d8c5a23d8c13ea91d75a73
pimlu <pimlu@users.noreply.github.com>
2015-04-11 09:34:32 -0400

    fix bugs in last row, add global.h
    my mistake.  looking into uninitialized value errors now.

---
+++ src/global.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef unsigned char bool;
+#define true 1
+#define false 0
+
+#define DOUL "\33[4m"
+#define NOUL "\33[0m"
