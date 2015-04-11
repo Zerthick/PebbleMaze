4a28996f0627de45c32743481e2e33d9f695b4aa
pimlu <pimlu@users.noreply.github.com>
2015-04-11 14:46:11 -0400

    make recursive maze generator use heap instead of stack
    the code is kind of silly, but it turns out that beautiful 24kb of ram
    is almost all for the heap/program memory, and just 2kb is for the
    stack.  thus, faux-heap callstack.

--- src/maze.c
+++ src/maze.c
@@ -8,53 +8,76 @@ BOOL istaken(Cell* maze, int w, int h, int x, int y) {
   return x>=0 && y>=0 && x<w && y<h && !maze[POS(y,x)].v;
 }
 
-void rec(Cell* maze, int w, int h, int x, int y) {
-  maze[POS(y,x)].v = TRUE;
-  int xopts[4] = {-1,1,0,0};
-  int yopts[4] = {0,0,-1,1};
-
-  //while we can, pick random options
-  while(TRUE) {
-    BOOL options[4];
-    int optionscount=0;
-    //we have to check each iteration what our options are, because the rec()
-    //calls below may have changed things
-    for(int i=0;i<4;i++) {
-      options[i]=istaken(maze,w,h,x+xopts[i],y+yopts[i]);
-      if(options[i]) optionscount++;
-    }
-    //quit if there's nothing to do
-    if(optionscount==0) return;
-
-    //make a list of possible directions
-    int choices[optionscount];
-    int choicecount=0;
-    for(int i=0;i<4;i++) {
-      if(options[i]) {
-        choices[choicecount++] = i;
-      }
-    }
-
-    //expand in whichever direction we can
-    int direction=choices[rand()%choicecount];
-    //now direction is the index of the direction we should take
-    if(xopts[direction]>0) maze[POS(y,x)].r = 0;
-    else if(xopts[direction]<0) maze[POS(y,x-1)].r = 0;
-    if(yopts[direction]>0) maze[POS(y,x)].b = 0;
-    else if(yopts[direction]<0) maze[POS(y-1,x)].b = 0;
-    rec(maze,w,h,x+xopts[direction],y+yopts[direction]);
-  }
-}
-
 Cell* genmaze(int w, int h) {
 
   Cell *maze = malloc(w*h*sizeof(*maze));
   maze[0] = (Cell) {1, 1, 0};
   memset(maze, *((char*) maze), w*h*sizeof(*maze));
 
-  rec(maze,w,h,0,0);
-
   maze[POS(h-1,w-1)].b = 0;
+
+  //false call stack
+  int *stack = malloc(w*h*2*sizeof(*stack));
+  int esp = 0;
+  int x=0, y=0;
+
+//special push/pops for our faux stack
+#define PUSH(a,b) stack[esp++]=(a); stack[esp++]=(b);
+#define POP(a,b) (b)=stack[--esp]; (a)=stack[--esp];
+
+  //faux recursive function
+  while(TRUE) {
+
+    int xopts[4] = {-1,1,0,0};
+    int yopts[4] = {0,0,-1,1};
+
+    //while we can, pick random options
+    while(TRUE) {
+      maze[POS(y,x)].v = TRUE;
+
+      BOOL options[4];
+      int optionscount=0;
+      //we have to check each iteration what our options are, because the rec()
+      //calls below may have changed things
+      for(int i=0;i<4;i++) {
+        options[i]=istaken(maze,w,h,x+xopts[i],y+yopts[i]);
+        if(options[i]) optionscount++;
+      }
+      //quit if there's nothing to do
+      if(optionscount==0) {
+        if(esp == 0) goto stop;//if we've actually finished everything, stop
+        //pop arguments off the call stack
+        POP(x,y);
+        continue;
+      }
+
+      //make a list of possible directions
+      int choices[optionscount];
+      int choicecount=0;
+      for(int i=0;i<4;i++) {
+        if(options[i]) {
+          choices[choicecount++] = i;
+        }
+      }
+
+      //expand in whichever direction we can
+      int direction=choices[rand()%choicecount];
+      //now direction is the index of the direction we should take
+      if(xopts[direction]>0) maze[POS(y,x)].r = 0;
+      else if(xopts[direction]<0) maze[POS(y,x-1)].r = 0;
+      if(yopts[direction]>0) maze[POS(y,x)].b = 0;
+      else if(yopts[direction]<0) maze[POS(y-1,x)].b = 0;
+      int newx=x+xopts[direction], newy=y+yopts[direction];
+      //"call" our recursive function for our new x and y
+      PUSH(newx,newy);
+      x=newx;y=newy;
+    }
+
+  }
+
+  stop:
+
+  free(stack);
 
   return maze;
 }
