9ac0211310bdc197be2dad058604cf6011e752ff
Zerthick <zerthick@gmail.com>
2015-04-11 09:34:26 -0400

    Added basic maze rendering framework.

--- src/main.c
+++ src/main.c
@@ -4,12 +4,17 @@
  * either modes of operation of the accelerometer.
  */
 
+#define POS(y,x,w) ((y)*w+(x))
+
 #include <pebble.h>
+#include "maze.h"
 
 static Window *s_main_window;
 static TextLayer *s_output_layer;
 static Layer *s_maze_layer;
 static Layer *s_player_layer;
+
+static Cell *maze;
 
 /*static void data_handler(AccelData *data, uint32_t num_samples) {
   // Long lived buffer
@@ -53,10 +58,27 @@ static Layer *s_player_layer;
   }
 }*/
 
+static void maze_layer_update_callback(Layer *layer, GContext *ctx){
+   for(int i = 0; i < 32; i++){
+     for(int j = 0; j < 32; j++){
+       int currentPos = POS(j, i, 32);
+       if(maze[currentPos].r) {
+         graphics_draw_line(ctx, GPoint(16*i, 16*j), GPoint(16*i, 16*j + 16));
+       }
+       if(maze[currentPos].b) {
+         
+       }
+     }
+   }
+}
+
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
-
+  
+  s_maze_layer = layer_create(window_bounds);
+  layer_set_update_proc(s_maze_layer, maze_layer_update_callback);
+  layer_add_child(window_layer, s_maze_layer);
   // Create output TextLayer
   /*s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
   text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
@@ -68,9 +90,13 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   // Destroy output TextLayer
   text_layer_destroy(s_output_layer);
+  layer_destroy(s_maze_layer);
 }
 
 static void init() {
+  // Create maze
+  maze = genMaze(32, 32);
+  
   // Create main Window
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -78,7 +104,6 @@ static void init() {
     .unload = main_window_unload
   });
   window_stack_push(s_main_window, true);
-  }
 }
 
 static void deinit() {
